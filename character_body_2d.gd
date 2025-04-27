6c544dd11df39eb845d1adda85e5afc0a1d5b9af
DopeSoap69 <luke_996@web.de>
2025-04-27 17:14:45 +0200

    Small-ish Update

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 110.0
 const JUMP_VELOCITY = -300.0
-
+var jumpcount = 0
+var maxjump = 2
 	
 
 func _physics_process(delta: float) -> void:
@@ -12,8 +13,11 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if is_on_floor():
+		jumpcount = 0
+	if Input.is_action_just_pressed("jump") and jumpcount < maxjump:
 		velocity.y = JUMP_VELOCITY
+		jumpcount += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,14 +25,32 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.x > 0:
-			$Sprite2D.flip_h = false
+			$AnimatedSprite2D.play("walk")
+			$AnimatedSprite2D.flip_h = false
 		elif velocity.x < 0:
-			$Sprite2D.flip_h = true
+			$AnimatedSprite2D.play("walk")
+			$AnimatedSprite2D.flip_h = true
 		if Input.is_action_pressed("run"):
 			velocity.x = direction * SPEED * 1.8
+
+
 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 
 	move_and_slide()
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+func _process(_delta):
+	if not is_on_floor():
+		_animated_sprite.play("jump")
+	elif is_on_floor() and Input.is_action_pressed("move_right"):
+		_animated_sprite.play("walk")
+	elif is_on_floor() and Input.is_action_pressed("move_left"):
+		_animated_sprite.play("walk")
+
+	
+	elif is_on_floor() and not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		_animated_sprite.play("sit")
